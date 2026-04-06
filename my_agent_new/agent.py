9cb7624d6c3615191143d0406711875fc799091c
Sreedhar Reddy Mukkamalla <47001846+Sreedharreddymukkamalla@users.noreply.github.com>
2026-04-06 14:54:49 -0400

    Configure agent: storyteller

--- my_agent_new/agent.py
+++ my_agent_new/agent.py
@@ -1,21 +1,11 @@
 from google.adk.agents.llm_agent import Agent
-from google.adk.tools.mcp_tool import McpToolset
-from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
+
 
 root_agent = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
-    description='An expert otaku assistant for discovering anime.',
+    name='storyteller',
+    description='Write story based on the outline provided by the user',
     instruction="""
-        You are an Anime Show Finder. Help users find anime based on 
-        genre, mood, or similar shows they enjoy. 
-        Always provide the Title, a brief synopsis, and where to watch.
-    """,
-    tools=[
-        McpToolset(
-            connection_params=StreamableHTTPConnectionParams(
-                url="https://aeo-mcp-server.amdal-dev.workers.dev/mcp",
-            ),
-        )
-    ],
+        Write story based on the outline provided by the user. It will be of 100 words shorter version as it is for a short film.
+    """
 )
